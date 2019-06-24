4dda9532594d756ebe80e45c94737526a71a9580
utshargoel1 <f2016023@Pilani.bits-pilani.ac.in>
2019-06-24 11:13:51 +0530

    networks built

--- components.py
+++ components.py
@@ -1,7 +1,19 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
+from tensorflow.compat.v1.layers import Flatten
+
 # from tf.nn import moments
 
 ## TODO: implement the use of spectral norm
+
+# instNormInit = tf.initializers.random_normal(mean=1.0, stddev=0.02)
+
+def up_sample(x, scale_factor=2):
+    _, h, w, _ = x.get_shape().as_list()
+    new_size = [h * scale_factor, w * scale_factor]
+    return tf.compat.v1.image.resize_nearest_neighbor(x, size=new_size)
 
 def l2_normalizer(v, eps=12-12):
     return v/(tf.reduce_sum(v**2)**0.5+eps)
@@ -13,7 +25,7 @@ def spectral_normed_weight(W, u=None, num_iters=1, update_collection=SPECTRAL_NO
     W_shape = W.shape.as_list()
     W_reshaped = tf.reshape(W, [-1, W_shape[-1]])
     if u is None:
-        u = tf.get_variable("u", [1, W_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
+        u = tf.compat.v1.get_variable("u", [1, W_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
     
     def power_iteration(i, u_i, v_i):
         v_ip1 = l2_normalizer(tf.matmul(u_i, tf.transpose(W_reshaped)))
@@ -31,85 +43,101 @@ def spectral_normed_weight(W, u=None, num_iters=1, update_collection=SPECTRAL_NO
     W_bar = W_reshaped / sigma
     W_bar = tf.reshape(W_bar, W_shape)
     
-    tf.add_to_collection(update_collection, u.assign(u_final))
+    tf.compat.v1.add_to_collection(update_collection, u.assign(u_final))
     return W_bar
 
-def adain(content, s_mean, s_std, eps=1e-5, data_format='channel_first'):
-    # assertlen(content.size()) >= 3
-    axes = [2,3] if data_format=='channel_first' else [1,2]
+def adain(content, s_mean, s_std, eps=1e-5, data_format='channels_last'):
+    axes = [2,3] if data_format=='channels_first' else [1,2]
+
     c_mean, c_var = tf.nn.moments(content, axes=axes, keep_dims=True)
-    #s_mean, s_var = tf.nn.moments(style, axes=axes, keep_dims=True)
-    # c_std, s_std = tf.sqrt(c_var), tf.sqrt(s_var)
     c_std = tf.sqrt(c_var)
-    
+
+    s_mean = tf.expand_dims(tf.expand_dims(s_mean, 1), 1)
+    s_std = tf.expand_dims(tf.expand_dims(s_std, 1), 1)
+
     return ((content-c_mean)/c_std)*s_std + s_mean
 
 def convLayer(x, out_ch, kernel_size, stride, padding=None, sn=True, scope="conv0"):
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
         k = kernel_size//2
         if padding is None:
-            padding = tf.constant([[k, k], [k, k]])
+            padding = tf.constant([[0,0], [k, k], [k, k], [0,0]])
         else:
-            padding = tf.constant([[padding, padding], [padding, padding]])
+            padding = tf.constant([[0,0], [padding, padding], [padding, padding], [0,0]])
         out = tf.pad(x, padding, "REFLECT")
-        w = tf.get_variable("kernel", shape=[kernel_size, kernel_size, out.get_Shape()[1], out_ch],
-            initializer=tf.contrib.layers.xavier_initializer())
-        out = tf.nn.conv2d(out, strides=[1,1,stride, stride], filter=spectral_normed_weight(w), padding='SAME', data_format="NCHW")
+        w = tf.compat.v1.get_variable("kernel", shape=[kernel_size, kernel_size, out.get_shape()[3], out_ch],
+            initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
+        b = tf.compat.v1.get_variable("b", [out_ch], initializer=tf.constant_initializer(0.0))
+        out = tf.nn.conv2d(out, strides=[1,stride, stride, 1], filter=w, padding='VALID', data_format="NHWC")
+        out = out+b
         return out
 
 def flattenHW(x):
-    return tf.reshape(x, shape=[x.shape[0], x.shape[1], -1])
+    return tf.reshape(x, shape=[-1, x.shape[2]*x.shape[1], x.shape[3]])
 
 def attention(x,ch, scope='attention', reuse=False):
-    with tf.variable_scope(scope, reuse=reuse):
-        f = tf.keras.layers.Conv2D(ch//8, kernel_size=1, stride=1, padding="valid") (x, ch//8, kernel_size=1, data_format="channels_first")(x)
-        g = tf.keras.layers.Conv2D(ch//8, kernel_size=1, stride=1, padding="valid") (x, ch//8, kernel_size=1, data_format="channels_first")(x)
-        h = tf.keras.layers.Conv2D(ch, kernel_size=1, stride=1, padding="valid") (x, ch//8, kernel_size=1, data_format="channels_first")(x)
+    with tf.compat.v1.variable_scope(scope, reuse=reuse):
+        f = tf.keras.layers.Conv2D(ch//8, kernel_size=1, strides=1, padding="valid", data_format="channels_last", 
+            kernel_initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02), bias_initializer=tf.constant_initializer(0.0))(x)
+        g = tf.keras.layers.Conv2D(ch//8, kernel_size=1, strides=1, padding="valid", data_format="channels_last",
+            kernel_initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02), bias_initializer=tf.constant_initializer(0.0))(x)
+        h = tf.keras.layers.Conv2D(ch, kernel_size=1, strides=1, padding="valid", data_format="channels_last",
+            kernel_initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02), bias_initializer=tf.constant_initializer(0.0))(x)
 
-        s = tf.matmul(flattenHW(g), flattenHW(f), transpose_b=True)
+        s = tf.matmul(flattenHW(f), flattenHW(g), transpose_b=True)
 
         beta = tf.nn.softmax(s)
         o = tf.matmul(beta, flattenHW(h))
 
-        gamma = tf.get_variable("gamma", [1], initializer=tf.constant_initializer(0.0))
-        o = tf.reshape(o, shape=x.shape)
+        gamma = tf.compat.v1.get_variable("gamma", [1], initializer=tf.constant_initializer(0.0))
+        o = tf.reshape(o, shape=[-1, x.shape[1], x.shape[2], x.shape[3]])
         x = gamma*o + x
         
         return x
 
-def residualBlockUp(x, out_ch, kernel_size=3, stride=1, upsample=None):
-    residual = x
-    residual = tf.keras.layers.UpSampling2D(size=upsample, interpolation="nearest", data_format="NCHW")(residual)
-    residual = convLayer(residual, out_ch, kernel_size=1, stride=1)
-    
-    out = tf.nn.relu(tf.contrib.instance_norm(x))
-    if upsample:
-        out =   tf.keras.layers.UpSampling2D(size=upsample, interpolation="nearest", data_format="NCHW")(out)
-    out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride)
-    out = tf.nn.relu(tf.contrib.instance_norm(out))
-    out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride)
-    
-    return residual + out
+def residualBlockUp(x, out_ch, kernel_size=3, stride=1, upsample=None, scope="resUp0"):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
+        residual = x
+        if upsample:
+            # residual = tf.keras.layers.UpSampling2D(size=upsample, interpolation="nearest", data_format="channels_last")(residual)
+            residual = up_sample(residual, upsample)
 
-def residualBlockDown(x, out_ch, kernel_size=3, stride=1, padding=None):
-    residual = x
-    residual = convLayer(residual, out_ch, kernel_size=1, stride=1)
-    residual = tf.keras.layers.AveragePooling2D(pool_size=2)(residual)
+        residual = convLayer(residual, out_ch, kernel_size=1, stride=1, scope= "___conv1")
+        
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(x, scope= "___instanceNorm1", data_format="NHWC"))
+        if upsample:
+            # out =   tf.keras.layers.UpSampling2D(size=upsample, interpolation="nearest", data_format="channels_last")(out)
+            out = up_sample(out, upsample)
+        out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, scope= "___conv2")
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(out, scope= "___instanceNorm2", data_format="NHWC"))
+        out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, scope= "___conv3")
+        return residual + out
 
-    out = tf.nn.relu(x)
-    out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
-    out = tf.nn.relu(out)
-    out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
-    out = tf.keras.layers.AveragePooling2D(pool_size=2)(out)
+def residualBlockDown(x, out_ch, kernel_size=3, stride=1, padding=None, scope="resDown0"):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
+        residual = x
+        residual = convLayer(residual, out_ch, kernel_size=1, stride=1, scope= "___conv1")
+        residual = tf.keras.layers.AveragePooling2D(pool_size=2)(residual)
 
-    return residual + out
+        out = tf.nn.relu(x)
+        out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, padding=padding, scope= "___conv2")
+        out = tf.nn.relu(out)
+        out = convLayer(out, out_ch, kernel_size=kernel_size, stride=stride, padding=padding, scope= "___conv3")
+        out = tf.keras.layers.AveragePooling2D(pool_size=2)(out)
 
-def residualBlock(x, channels):
-    residual = x
-    out= tf.nn.relu(tf.contrib.layers.instance_norm(convLayer(x, channels, kernel_size=3, stride=1)))
-    out= tf.contrib.layers.instance_norm(convLayer(x, channels, kernel_size=3, stride=1))
-    out = tf.nn.relu(out + residual)
-    return out
+        return residual + out
+
+def residualBlock(x, channels, scope="resBlock0"):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
+        residual = x
+
+        out = convLayer(x, channels, kernel_size=3, stride=1, scope="___conv1")
+        out= tf.nn.relu(tf.contrib.layers.instance_norm(out, scope="___instanceNorm1", data_format="NHWC"))
+        out = convLayer(out, channels, kernel_size=3, stride=1, scope="___conv2")
+        out= tf.contrib.layers.instance_norm(out, scope="___instanceNorm2", data_format="NHWC")
+
+        out = tf.nn.relu(out + residual)
+        return out
 
 
 
--- network.py
+++ network.py
@@ -1,35 +1,29 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-from .components import residualBlock, residualBlockUp, residualBlockDown, adain, attention, convLayer
+from components import residualBlock, residualBlockUp, residualBlockDown, adain, attention, convLayer
 import tensorflow as tf
 import keras as K
 from collections import OrderedDict
-
 # TODO: wieight inits
 
-# class Emebedder(object):
 
-#     def __init__():
-#         res1 = 
+def embedder(x, y, scope="embedder"):
+    with tf.compat.v1.variable_scope(scope):
 
-def embedder(x, y):
-    assert x.shape == y.shape
-    out = tf.concat(x, y, axis=0)
-    out = out.expand_dims(out, 0)
+        out = tf.concat([x, y], axis=3)
+        out = tf.nn.relu(residualBlockDown(out, 64, scope= "___resDown1"))
+        out = tf.nn.relu(residualBlockDown(out, 128, scope= "___resDown2"))
+        out = tf.nn.relu(residualBlockDown(out, 256, scope= "___resDown3"))
+        out = attention(out, 256, scope= "___attn")
+        out = tf.nn.relu(residualBlockDown(out, 512, scope= "___resDown4"))
+        
+        out = tf.nn.relu(K.layers.GlobalMaxPooling2D(data_format="channels_last")(out))
+        return out
 
-    out = tf.nn.relu(residualBlockDown(out, 64))
-    out = tf.nn.relu(residualBlockDown(out, 128))
-    out = tf.nn.relu(residualBlockDown(out, 256))
-    out = attention(out, 256)
-    out = tf.nn.relu(residualBlockDown(out, 512))
-    
-    out = tf.nn.relu(K.layers.GlobalMaxPooling2D(data_format="channels_first")(out).reshape(512, 1))
+def generator(y, e, scope="generator"):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
 
-    return out
-
-def generator(y, e):
-    with tf.variable_scope("generator", reuse=tf.AUTO_REUSE):
-        assert(y.shape) == [3,224, 224]
-        psi_width = 32
         adain_layers = OrderedDict([
             ('deconv4', 256),
             ('deconv3', 128),
@@ -40,66 +34,77 @@ def generator(y, e):
         out = {}
         start, end = 0, 0
         for layer in adain_layers:
-            end = start + d[layer]*2
+            end = start + adain_layers[layer]*2
             out[layer] = (start, end)
             start = end
-        projection = tf.get_variable("projection", [psi_length, 512])
+        projection = tf.compat.v1.get_variable("projection", [end, 512], dtype=tf.float32)
 
-        def slice_psi(psi, portion):
-            start, end = out[portion]
-            a = tf.reshape(psi[start:end], (2, -1))
-            return tf.reshape(a[0], [1,-1,1,1]),  tf.reshape(a[1], [1,-1,1,1])
+        out = y
 
-        out = tf.expand_dims(y, 0)
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(residualBlockDown(out, 64, scope= "___resDown1"), data_format="NHWC"))
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(residualBlockDown(out, 128, scope= "___resDown2"), data_format="NHWC"))
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(residualBlockDown(out, 256, scope= "___resDown3"), data_format="NHWC"))
+        out = attention(out, 256, scope= "___attn1")
+        out = tf.nn.relu(tf.contrib.layers.instance_norm(residualBlockDown(out, 512, scope= "___resDown4"), data_format="NHWC"))
 
-        out = tf.nn.relu(tf.contrib.instance_norm(residualBlockDown(out, 64)))
-        out = tf.nn.relu(tf.contrib.instance_norm(residualBlockDown(out, 128)))
-        out = tf.nn.relu(tf.contrib.instance_norm(residualBlockDown(out, 256)))
-        out = attention(out, 256)
-        out = tf.nn.relu(tf.contrib.instance_norm(residualBlockDown(out, 512)))
+        out = residualBlock(out, 512, scope= "___resBlock1")
+        out = residualBlock(out, 512, scope= "___resBlock2")
+        out = residualBlock(out, 512, scope= "___resBlock3")
+        out = residualBlock(out, 512, scope= "___resBlock4")
+        out = residualBlock(out, 512, scope= "___resBlock5")
+
+        psi_hat = tf.matmul(e, projection, transpose_b=True)
+
+        out = residualBlockUp(out, 256, upsample=2, scope= "___resUp1")
+        out = tf.nn.relu(adain(out, tf.slice(psi_hat, [0, 0], [-1, 256]), tf.slice(psi_hat, [0, 256], [-1, 256])))
+        out = residualBlockUp(out, 128, upsample=2, scope= "___resUp2")
+        out = tf.nn.relu(adain(out, tf.slice(psi_hat, [0, 512], [-1, 128]), tf.slice(psi_hat, [0, 640], [-1, 128])))
         
-        out = residualBlock(out, 512)
-        out = residualBlock(out, 512)
-        out = residualBlock(out, 512)
-        out = residualBlock(out, 512)
-        out = residualBlock(out, 512)
+        out = attention(out, 128, scope= "___attn2")
 
-        psi_hat = tf.reshape(tf.matmul(projection, e), -1)
+        out = residualBlockUp(out, 64, upsample=2, scope= "___resUp3")
+        out = tf.nn.relu(adain(out, tf.slice(psi_hat, [0, 768], [-1, 64]), tf.slice(psi_hat, [0, 832], [-1, 64])))
+        out = residualBlockUp(out, 3, upsample=2, scope= "___resUp4")
+        out = tf.nn.relu(adain(out, tf.slice(psi_hat, [0, 896], [-1, 3]), tf.slice(psi_hat, [0, 899], [-1, 3])))
 
-        out = residualBlockUp(out, 256, upsample=2)
-        out = tf.nn.relu(adain(out, *slice_psi(psi_hat, 'deconv4')))
-        out = residualBlockUp(out, 128, upsample=2)
-        out = tf.nn.relu(adain(out, *slice_psi(psi_hat, 'deconv3')))
+    return out
+
+
+def discriminator(ntv, x, y, e=None, i=None,scope="discriminator"):
+    with tf.compat.v1.variable_scope(scope, reuse=False):
+        out = tf.concat([x, y], axis=3)
+        # out = tf.expand_dims(out, 0)
         
-        out = attention(out, 128)
+        out0 = tf.nn.relu(residualBlockDown(out, 64, scope= "___resDown1"))
+        out1 = tf.nn.relu(residualBlockDown(out0, 128, scope= "___resDown2"))
+        out2 = tf.nn.relu(residualBlockDown(out1, 256, scope= "___resDown3"))
+        out3 = attention(out2, 256, scope= "___attn")
+        out4 = tf.nn.relu(residualBlockDown(out3, 512, scope= "___resDown4"))
 
-        out = residualBlockUp(out, 64, upsample=2)
-        out = tf.nn.relu(adain(out, *slice_psi(psi_hat, 'deconv2')))
-        out = residualBlockUp(out, 3, upsample=2)
-        out = tf.nn.relu(adain(out, *slice_psi(psi_hat, 'deconv1')))
+        out = residualBlock(out4, 512, scope= "___resBlock1")
 
-    return out[0]
+        out = tf.nn.relu(K.layers.GlobalMaxPooling2D(data_format="channels_last")(out))
+        print(out.shape)
+        W = tf.compat.v1.get_variable( "___W", shape=(ntv, 512,), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
+        w0 = tf.compat.v1.get_variable( "___w0", shape=(1, 512), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
+        b = tf.compat.v1.get_variable( "___b", shape=(1,), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
+        if i is None:
+            out = tf.reduce_sum(e*out, axis=1) + b
+        else:
+            out = tf.reduce_sum((tf.nn.embedding_lookup(W,i)+ w0)*out, axis=1) + b
 
-
-def discriminator(ntv, x, y, i):
-    with tf.variable_scope("discriminator", reuse=tf.AUTO_REUSE):
-        out = tf.concat(x, y, axis=0)
-        out = tf.expand_dims(out, 0)
-        
-        out0 = tf.nn.relu(residualBlockDown(out, 64))
-        out1 = tf.nn.relu(residualBlockDown(out0, 128))
-        out2 = tf.nn.relu(residualBlockDown(out1, 256))
-        out3 = attention(out2, 256)
-        out4 = tf.nn.relu(residualBlockDown(out3, 512))
-
-        out = residualBlock(out4, 512)
-
-        out = tf.nn.relu(K.layers.GlobalMaxPooling2D(data_format="channels_first")(out).reshape(512, 1))
-
-        W = tf.get_variable("W", shape=(512, ntv), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
-        w0 = tf.get_variable("w0", shape=(512, 1), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
-        b = tf.get_variable("b", shape=(1,), initializer=tf.initializers.random_normal(mean=0.0, stddev=0.02))
-
-        tf.matmul(out, tf.slice())
+        out = tf.nn.tanh(out)
+        return out, [out0, out1, out2, out3, out4]
 
 
+
+
+if __name__ == "__main__":
+    x = tf.compat.v1.placeholder(tf.float32, [None, 224, 224, 3])
+    y = tf.compat.v1.placeholder(tf.float32, [None, 224, 224, 3])
+    e = tf.compat.v1.placeholder(tf.float32, [None, 512])
+    i = tf.compat.v1.placeholder(tf.int32, [None])
+    # e = embedder(x,y)
+
+    # g = generator(y, e)
+    d = discriminator(1000, x, y, e, i)
